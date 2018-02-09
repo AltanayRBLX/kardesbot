cad6c75b9419df15123ce8b9cbb343df1f6bebbb
AltanayRBLX <35113904+AltanayRBLX@users.noreply.github.com>
2018-02-09 18:22:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -186,7 +186,7 @@ bot.on("message", function(message) {
                 }
             }
             break
-	case "çağır"
+	case "çağır":
             bot.users.get('315725595554086912').send('**seni **' + message.channel + ' **kanalından** ' + message.author + ' **çağrıyo.**')
             bot.users.get('293006152692662273').send('**seni **' + message.channel + ' **kanalından** ' + message.author + ' **çağrıyo.**')
         case "slots":
